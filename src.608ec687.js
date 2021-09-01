301894cb0d6b5aa0c062163912790a903c028d6e
VikOrlovskiy <86106913+VikOrlovskiy@users.noreply.github.com>
2021-09-01 13:02:42 +0000

    Deploying to gh-pages from @ VikOrlovskiy/test-team-projeckt@8b1cf586428f1a4c5ff5f9975ea1dd45a0ab20d3 🚀

--- src.608ec687.js
+++ src.608ec687.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/test-team-projeckt/src.89a2ac89.js.map
+//# sourceMappingURL=/test-team-projeckt/src.608ec687.js.map
